a20c42885fdbca966665abb94e6727f6dfb2c96d
kenn <kennnguyen@usf.edu>
2025-01-24 23:53:06 -0500

    task 2 - product details

--- cc_1.js
+++ cc_1.js
@@ -2,6 +2,14 @@
 let employeeName = "Captain Rex"; 
 const employeeID = 1;
 var isActive = true;
-console.log(typeof employeeName)
-console.log(typeof employeeID)
-console.log(typeof isActive)
+console.log(typeof employeeName);
+console.log(typeof employeeID);
+console.log(typeof isActive);
+
+//task 2 : primitive data types
+let productName = "credits";
+const productPrice = 25;
+var isAvailable = true; 
+console.log(typeof productName);
+console.log(typeof productPrice);
+console.log(typeof isAvailable); 
