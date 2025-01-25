f20c61342c504a3bb809aebc8e25820ab8241c85
kenn <kennnguyen@usf.edu>
2025-01-24 23:49:27 -0500

    task 1 - employee information

---
+++ cc_1.js
@@ -0,0 +1,7 @@
+// task 1 : variables 
+let employeeName = "Captain Rex"; 
+const employeeID = 1;
+var isActive = true;
+console.log(typeof employeeName)
+console.log(typeof employeeID)
+console.log(typeof isActive)
